58b0629a74944cb96d03995f6b9fa8babf03e19d
Emir Arda Tomac <144858927+emirardatomac@users.noreply.github.com>
2025-07-05 18:45:40 +0300

    Add files via upload

---
+++ 3_4_LogisticRegression.py
@@ -0,0 +1,29 @@
+from ucimlrepo import fetch_ucirepo
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+import warnings
+warnings.filterwarnings("ignore")
+
+# fetch dataset 
+heart_disease = fetch_ucirepo(id=45) 
+  
+df = pd.DataFrame(data = heart_disease.data.features)
+
+df["target"] = heart_disease.data.targets
+
+if df.isna().any().any():
+    df.dropna(inplace=True)
+    print("nan")
+    
+X = df.drop(["target"],axis=1).values
+y = df.target.values
+
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.1,random_state=42)
+
+log_Reg = LogisticRegression(penalty="l2", C = 1 , solver="lbfgs",max_iter=100)
+log_Reg.fit(X_train,y_train)
+
+accuracy = log_Reg.score(X_test, y_test)
+print("Logistic Regression Acc:" , accuracy)
+
